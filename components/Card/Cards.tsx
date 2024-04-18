02f0e2d8671bb6abf1682d96a5e35c141970dea5
Jacob Valdez <jacobv031@gmail.com>
2024-04-18 18:22:19 -0500

    removed white space

--- components/Card/Cards.tsx
+++ components/Card/Cards.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-
 import { CardActionArea, CardActions } from "@mui/material";
 
 const Cards = () => {
