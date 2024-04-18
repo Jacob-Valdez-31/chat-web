0424ea6103f106ceff767a9efe29f812b67d7b30
Jacob Valdez <jacobv031@gmail.com>
2024-04-18 18:22:07 -0500

    Cleaned up unused imports

--- components/Card/Cards.tsx
+++ components/Card/Cards.tsx
@@ -2,8 +2,8 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions } from "@mui/material";
+
+import { CardActionArea, CardActions } from "@mui/material";
 
 const Cards = () => {
   return (
