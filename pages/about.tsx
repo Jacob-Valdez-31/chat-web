cae822d72c939721a355cd0c7b6c91736773db98
Jacob Valdez <jacobv031@gmail.com>
2024-04-18 20:44:06 -0500

    Created about page

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import About from "@/components/About";
+import AboutHero from "@/components/Heros/AboutHero";
+import Locations from "@/components/Location";
+
+const Home = () => {
+  return (
+    <div>
+      <AboutHero />
+      <Locations />
+      <About />
+    </div>
+  );
+};
+
+export default Home;
