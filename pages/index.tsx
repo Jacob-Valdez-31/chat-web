7403de297e845162d4f8b05082902fd340f8f474
Jacob Valdez <jacobv031@gmail.com>
2024-04-21 17:15:30 -0500

    Changed styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,27 @@
 import CardTwo from "@/components/Card/CardTwo";
 import Hero from "@/components/Heros/Hero";
 import CardOne from "@/components/Card/CardOne";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 700);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+  }, []);
   return (
     <div>
       <Hero />
-      <div className="flex mt-[100px] mb-[50px]">
+      <div
+        className={
+          isMobile
+            ? "sm:flex-row mt-[100px] mb-[50px] sm:mt-[150px] w-[300px] mx-auto"
+            : "flex mt-[150px] mb-[50px] justify-center gap-[100px] md:gap-8 lg:gap-[100px] "
+        }
+      >
         <CardOne />
         <CardTwo />
       </div>
