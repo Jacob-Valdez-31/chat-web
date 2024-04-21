ca072123d6433ef59febd5eb922629c16af93d07
Jacob Valdez <jacobv031@gmail.com>
2024-04-21 17:14:16 -0500

    Created resume component

---
+++ components/Resume.tsx
@@ -0,0 +1,20 @@
+const Resume = () => {
+  return (
+    <div className="resume">
+      <div className="container">
+        <h2 id="3">Resume</h2>
+        <p>
+          Loren ipsum dolor site amet,ea domain until epicuri iudicabiit nam ,
+          te usu virtute placerat. purto burte disputando cu est, Loren ipsum
+          dolor site amet,ea domain until epicuri iudicabiit nam , te usu
+          virtute placerat. purto burte disputando cu est.
+        </p>
+
+        <a href="/resume.pdf" download="Jacob_Valdez_Resume">
+          <button type="button">Download Resume</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+export default Resume;
