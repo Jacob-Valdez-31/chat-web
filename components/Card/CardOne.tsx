9abc68df0aa07aca3d8c2338b3bedd318b59a8bc
Jacob Valdez <jacobv031@gmail.com>
2024-04-21 17:14:45 -0500

    Changed styling

--- components/Card/CardOne.tsx
+++ components/Card/CardOne.tsx
@@ -6,11 +6,11 @@ import { CardActionArea, CardActions } from "@mui/material";
 
 const CardOne = () => {
   return (
-    <Card className="max-w-[345px] md:h-[545px] m-auto mt-2">
+    <Card className="max-w-[345px]  md:h-[425px] mt-5">
       <CardMedia
         component="img"
         image="/img/chat-gif.gif"
-        className="h-[300px] border-b"
+        className="h-[200px] border-b"
       />
       <CardContent>
         <h1>Open Chat Room</h1> Lorem ipsum dolor, sit amet consectetur
