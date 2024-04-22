acc407d1b44fee3a908ef0c589a11632ffc5cb37
Jacob Valdez <jacobv031@gmail.com>
2024-04-22 18:43:05 -0500

    Updated styling

--- pages/chat-page.tsx
+++ pages/chat-page.tsx
@@ -3,12 +3,11 @@ import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatPage = (props: any) => {
   return (
-    <div style={{ height: "900px" }}>
+    <div className="h-[900px]">
       <PrettyChatWindow
         projectId="59570cd9-e5c7-4805-8f8f-d8b0f1c146bf"
         username={props.user.username}
         secret={props.user.secret}
-        style={{ height: "900px" }}
       />
     </div>
   );
