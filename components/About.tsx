8e4b1a19b3825fc2126abe8a39cb33fe2393a4dc
Jacob Valdez <jacobv031@gmail.com>
2024-04-29 18:16:22 -0500

    updated  about me copy

--- components/About.tsx
+++ components/About.tsx
@@ -17,12 +17,22 @@ const About = () => {
               About Me
             </h1>
             <p>
-              Lorem ipsum dolor sit amet, ea doming epicuri iudicabit nam, te
-              usu virtute placerat.
+              Good to see you've made it this far! My name is Jacob Valdez and
+              next to me is my beautiful wife Hannah. We recently just got
+              married and as I said earlier live out in Conway, Arkansas (well
+              kinda lol). Hannah just finished getting her degree in education
+              to be a school teacher and will be graduating from UCA in may of
+              2024.
             </p>
+
             <p>
-              Lorem ipsum dolor sit amet, ea doming epicuri iudicabit nam, te
-              usu virtute placerat.
+              As for myself I am originally from Texas. I went to Texas A&M
+              where I graduated with a degree in Sport Management. Funny to
+              think that I now consider my self a software developer but doesn't
+              everyone do something else then what there degree is in??? Its
+              crazy to think how life itself will take us on unexpected paths.
+              Nonetheless I am grateful for this live I get to live with my
+              beautiful wife by my side.
             </p>
           </div>
         </div>
