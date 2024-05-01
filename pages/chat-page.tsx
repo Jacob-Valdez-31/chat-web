370369c662a43bfb27e9a37a32629e2f2f53522c
Jacob Valdez <jacobv031@gmail.com>
2024-05-01 12:34:55 -0500

    Fixed styling of the height

--- pages/chat-page.tsx
+++ pages/chat-page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+
 import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatPage = (props: any) => {
   return (
-    <div className="h-[900px]">
+    <div className="h-[850px]">
       <PrettyChatWindow
         projectId="59570cd9-e5c7-4805-8f8f-d8b0f1c146bf"
         username={props.user.username}
